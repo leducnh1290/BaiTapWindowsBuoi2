a7ec16da20ca7d7106eb4d2d9d1fb2adfa04555c
leducnh1290 <leducanh1290@gmail.com>
2024-11-20 16:13:56 +0700

    Add project files.

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+         List<Student> students = new List<Student>();  
+            students.Add(new Student(1,"Anh",17));
+            students.Add(new Student(2,"Hue",20));
+            students.Add(new Student(3,"Bao",50));
+            students.Add(new Student(4,"Duong",40));
+            students.Add(new Student(5,"Ly",70));
+            Console.WriteLine("a. Danh sach toan bo sv:");
+            foreach (var student in students)
+            {
+                Console.WriteLine($"Ten: {student.Name}, Tuoi: {student.Age}");
+            }
+
+           
+            Console.WriteLine("\nb. SV co tuoi tu 15 den 18");
+            var ageRange = students.Where(s => s.Age >= 15 && s.Age <= 18).ToList();
+            foreach (var student in ageRange)
+            {
+                Console.WriteLine($"Ten: {student.Name}, Tuoi: {student.Age}");
+            }
+
+           
+            Console.WriteLine("\nc. Sv co ten bat dau bang 'A':");
+            var nameStartsWithA = students.Where(s => s.Name.StartsWith("A")).ToList();
+            foreach (var student in nameStartsWithA)
+            {
+                Console.WriteLine($"Ten: {student.Name}, Tuoi: {student.Age}");
+            }
+
+          
+            Console.WriteLine("\nd. Tong tat ca tuoi cua toan bo sv:");
+            var totalAge = students.Sum(s => s.Age);
+            Console.WriteLine($"Tong tuoi: {totalAge}");
+
+         
+            Console.WriteLine("\ne. Hoc sinh co tuoi lon nhat:");
+            var oldestStudent = students.OrderByDescending(s => s.Age).FirstOrDefault();
+            if (oldestStudent != null)
+            {
+                Console.WriteLine($"Ten: {oldestStudent.Name}, Tuoi: {oldestStudent.Age}");
+            }
+
+            
+            Console.WriteLine("\nf. Sap xep sv theo tuoi tang dan:");
+            students.OrderBy(s => s.Age).ToList().ForEach(x=>Console.WriteLine(x));
+
+        }
+    }
+
+}
---
+++ ConsoleApp1/Student.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    internal class Student
+    {
+        public int Id { get; set; }
+        public String Name { get; set; }
+        public int Age { get; set; }
+        public Student()
+        {
+            this.Id = 0;
+            this.Age = 0;
+            this.Name = "Duc Anh";
+        }
+        public Student(int id, string name, int age)
+        {
+            Id = id;
+            Name = name;
+            Age = age;
+        }
+        public override string ToString()
+        {
+            return $"Student [MaSv: {Id}, TenSv: {Name}, TuoiSv: {Age}]";
+        }
+    }
+}
